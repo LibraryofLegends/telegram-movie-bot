d6e4494520b305b5c18abc60b7aed41ad683b5f4
LibraryofLegends <tlorenz.iphone12@gmail.com>
2026-04-23 18:50:25 +0200

    index.js aktualisieren

--- index.js
+++ index.js
@@ -1,22 +1,3 @@
-const fetch = global.fetch || require("node-fetch");
-const express = require("express");
-const fs = require("fs");
-
-const app = express();
-app.use(express.json());
-
-const TOKEN = process.env.TOKEN;
-const TMDB_KEY = process.env.TMDB_KEY;
-const CHANNEL_ID = process.env.CHANNEL_ID;
-const BOT_USERNAME = process.env.BOT_USERNAME || "LIBRARY_OF_LEGENDS_Bot";
-
-const DB_FILE = "films.json";
-const HISTORY_FILE = "history.json";
-
-// ================= GLOBAL UI STATE =================
-global.LAST_LIST = null;
-global.LAST_HEADING = "";
-
 // ================= DB =================
 function loadDB() {
   if (!fs.existsSync(DB_FILE)) return [];
